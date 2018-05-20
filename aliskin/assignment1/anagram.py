cdeb07c97f0f9af00b917e2df4368f4f498c0580
Alisa Smirnova <aliskin@exascale.info>
2018-05-21 00:01:16 +0200

    Update anagram.py

--- aliskin/assignment1/anagram.py
+++ aliskin/assignment1/anagram.py
@@ -1,17 +1,19 @@
-def isAnagram(s, t, case_sensitive=False):
+from collections import defaultdict
+
+def isAnagram(s, t, case_sensitive=True):
     """
     Given two strings s and t returns True if t is anagram of s and False otherwise.
     """
     if len(s) != len(t):
         return False
-    symbols = [0 for i in range(256)]
-    if (!case_sensitive):
+    if (not case_sensitive):
         s = s.casefold()
         t = t.casefold()
+    symbols = defaultdict(int)
     for c in s:
-        symbols[ord(c)] += 1
+        symbols[c] += 1
     for c in t:
-        symbols[ord(c)] -= 1
-        if symbols[ord(c)] < 0:
+        symbols[c] -= 1
+        if symbols[c] < 0:
             return False
     return True

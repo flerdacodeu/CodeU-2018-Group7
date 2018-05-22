9f8b9bf6616c17705fbb7603cc57f9abddb31f8e
Alisa Smirnova <aliskin@exascale.info>
2018-05-22 18:25:30 +0200

    Added two examples

--- aliskin/assignment1/kth-element.py
+++ aliskin/assignment1/kth-element.py
@@ -17,3 +17,22 @@ def kth_last_element(head, k):
         current = current.next
         i += 1
     return current
+
+head = ListNode(1)
+prev = head
+for i in range(2, 6):
+    new_node = ListNode(i)
+    prev.next = new_node
+    prev = new_node
+
+print(kth_last_element(head, 0).value)
+
+letters = "abcdefghijklmnopqrstuwxyz"
+head = ListNode('a')
+prev = head
+for symbol in letters[1:]:
+    new_node = ListNode(symbol)
+    prev.next = new_node
+    prev = new_node
+
+print(kth_last_element(head, 4).value)

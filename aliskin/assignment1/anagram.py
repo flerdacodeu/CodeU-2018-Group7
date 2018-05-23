0c379e8615d1dbe356019cc756a49ae676dd66c2
Smirnova Alisa <aliskin@exascale.info>
2018-05-23 09:50:30 +0200

    Replace print with assert

--- aliskin/assignment1/anagram.py
+++ aliskin/assignment1/anagram.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 from collections import Counter
+import string
 
 def is_anagram(s, t, case_sensitive=True):
     """
@@ -18,12 +19,24 @@ def main():
     """
     Some simple examples to test the function is_anagram
     """
-    if is_anagram('a', 'A'):
-        print('There is a bug')
-    if not is_anagram('a', 'A', False):
-        print('There is a bug')
-    if not is_anagram('apple', 'lappe'):
-        print('There is a bug')
+    test_examples = [('', '', True), 
+            ('a', 'A', False),
+            ('a', 'aa', False),
+            ('apple', 'lappe', True),
+            (string.ascii_lowercase, string.ascii_lowercase, True),
+            (string.ascii_lowercase, string.ascii_uppercase, False)]
+    for s, t, answer in test_examples:
+        assert (is_anagram(s, t) == answer), 'Incorrect answer for {} and {}, should be {}'.format(s, t, answer)
+    """
+    Check case insensitive
+    """
+    test_examples = [('', '', True),
+            ('a', 'A', True),
+            ('ABC', 'Cabbba', False),
+            (string.ascii_lowercase, string.ascii_lowercase, True),
+            (string.ascii_lowercase, string.ascii_uppercase, True)]
+    for s, t, answer in test_examples:
+        assert (is_anagram(s, t, case_sensitive=False) == answer), 'Incorrect answer for {} and {}, should be {}'.format(s, t, answer)
 
 if __name__ == '__main__':
     main()

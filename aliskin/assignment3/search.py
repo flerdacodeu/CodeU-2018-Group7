0714d7e0d7235e567e64de0d9ef1f2089d0ac192
Smirnova Alisa <aliskin@exascale.info>
2018-06-19 15:53:51 +0100

    Fixed review comments
    Merged ifs; Made word_search_with_prefix function a closure

--- aliskin/assignment3/search.py
+++ aliskin/assignment3/search.py
@@ -13,17 +13,20 @@ def get_valid_neighbours(position, n_rows, n_cols):
     row, col = position
     for i in range(row - 1, row + 2):
         for j in range(col - 1, col + 2):
-            if (i == row) and (j == col):
-                continue
-            if is_valid_index(i, n_rows) and is_valid_index(j, n_cols):
+            if (i != row or j != col) and is_valid_index(i, n_rows) and is_valid_index(j, n_cols):
                 neighbours.append((i, j))
     return neighbours
 
-def word_search_with_prefix(prefix, grid, dictionary, visited, queue):
+def word_search_with_prefix(grid, dictionary, queue, prefix=None, visited=None):
     '''
     Recursively searches the words from dictionary given a grid and a prefix
     Returns a set of words
     '''
+    if prefix is None:
+        prefix = ''
+    if visited is None:
+        visited = set()
+
     words = set()
     while len(queue) > 0:
         row, col = queue.popleft()
@@ -38,8 +41,7 @@ def word_search_with_prefix(prefix, grid, dictionary, visited, queue):
         visited_new.add((row, col))
         to_visit = deque(get_valid_neighbours((row, col), len(grid),
             len(grid[0])))
-        words = words.union(word_search_with_prefix(prefix_new, grid, dictionary,
-            visited_new, to_visit))
+        words = words.union(word_search_with_prefix(grid, dictionary, to_visit, prefix_new, visited_new))
     return words
 
 def word_search(grid, dictionary):
@@ -57,11 +59,8 @@ def word_search(grid, dictionary):
     n_cols = len(grid[0])
     for row in range(n_rows):
         for col in range(n_cols):
-            visited = set()
-            prefix = ''
             queue = deque([(row, col)])
-            words = words.union(word_search_with_prefix(prefix, grid, dictionary,
-                visited, queue))
+            words = words.union(word_search_with_prefix(grid, dictionary, queue))
             if len(words) == len(dictionary.words):
                 return words
     return words

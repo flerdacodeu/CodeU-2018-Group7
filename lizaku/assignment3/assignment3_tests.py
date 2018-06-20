a438b1f6d8e7d08863ae7debd97b6d985dd5babf
Elizaveta Kuzmenko <7359847+lizaku@users.noreply.github.com>
2018-06-20 03:08:16 +0200

    add tests for nonexisting words

--- lizaku/assignment3/assignment3_tests.py
+++ lizaku/assignment3/assignment3_tests.py
@@ -12,20 +12,24 @@ class Test(unittest.TestCase):
     def test_is_word(self):
         self.assertTrue(self.vocab.isWord('cat'))
         self.assertFalse(self.vocab.isWord('c'))
+        self.assertFalse(self.vocab.isWord('catt'))
 
     def test_is_prefix(self):
         self.assertTrue(self.vocab.isPrefix('ca'))
         self.assertFalse(self.vocab.isPrefix('at'))
+        self.assertFalse(self.vocab.isPrefix('catt'))
         
 
     # tests the main function
     def test_find_words(self):
         self.assertSetEqual(find_words(self.grid, self.vocab), {'card', 'car', 'cat', 'a'})
-
+    
+    # test if the Dictionary is empty
     def test_empty_dict(self):
         vocab = Dictionary([])
         self.assertEqual(find_words(self.grid, vocab), set())
 
+    # test if the Grid is empty
     def test_empty_grid(self):
         grid = Grid([[]])
         self.assertEqual(find_words(grid, self.vocab), set())

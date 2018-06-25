8452bd5e7a9c13b91ff5e2e9aacbe1e7fca6b62c
Rakhilya Lee Mekhtieva <rsml@fb.com>
2018-06-25 20:49:17 +0100

    Added tests for Q2

--- qvks/assignment1/Q2.py
+++ qvks/assignment1/Q2.py
@@ -3,11 +3,11 @@ class Node:
 		self.value = x
 		self.next = None
 
-def get_kth_last_element(k):
+def get_kth_last_element(self, k):
 	if k<0:
 		return None
-	curr1 = self.head
-	curr2 = self.head
+	curr1 = self
+	curr2 = self
 	for i in range (k+1):
 		if not curr1:
 			return None
@@ -16,3 +16,18 @@ def get_kth_last_element(k):
 		curr1 = curr1.next
 		curr2 = curr2.next
 	return curr2.value
+
+def main():
+	n0 = Node(0)
+	n1 = Node(1)
+	n2 = Node(2)
+
+	n0.next = n1
+	n1.next = n2
+	assert(get_kth_last_element(n0, 1) == 1) 
+	assert(get_kth_last_element(n0, 2) == 0) 
+	assert(not get_kth_last_element(n0, 3)) 
+
+main()
+
+main()

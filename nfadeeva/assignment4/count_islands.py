990e756c3134e3fa53b87cc682bb7139b8a0f60c
afadeeva <fadeevanastasiia@gmail.com>
2018-07-03 00:34:23 +0300

    Add fixes

--- nfadeeva/assignment4/count_islands.py
+++ nfadeeva/assignment4/count_islands.py
@@ -1,27 +1,27 @@
 from typing import List
 
 
-def neighbors(input_array, num_rows, num_cols, x, y):
+def neighbors(tiles, num_rows, num_cols, x, y):
     for dx, dy in [(-1, 0), (0, -1), (0, 1), (1, 0)]:
             xn, yn = x + dx, y + dy
             if (0 <= xn < num_rows and 0 <= yn < num_cols and
-                    input_array[xn][yn]):
+                    tiles[xn][yn]):
                 yield xn, yn
 
 
-def dfs(num_rows, num_cols, input_array, x, y):
+def dfs(num_rows, num_cols, tiles, x, y):
 
     # mark the current tile as visited
-    input_array[x][y] = False
+    tiles[x][y] = False
     # start dfs from every neighbour of the current tile
-    for next_x, next_y in neighbors(input_array, num_rows, num_cols, x, y):
-        dfs(num_rows, num_cols, input_array, next_x, next_y)
+    for next_x, next_y in neighbors(tiles, num_rows, num_cols, x, y):
+        dfs(num_rows, num_cols, tiles, next_x, next_y)
 
 
-def count_islands(num_rows, num_cols, input_array: List[bool]):
+def count_islands(num_rows, num_cols, tiles: List[List[bool]]):
     """
     Count islands in the given 2d array (List)
-    :param input_array: input array, where True is land, False is water
+    :param tiles: input array, where True is land, False is water
     :return: num of islands
     """
 
@@ -30,7 +30,7 @@ def count_islands(num_rows, num_cols, input_array: List[bool]):
     for x in range(num_rows):
         for y in range(num_cols):
             # the new connected component is started
-            if input_array[x][y]:
+            if tiles[x][y]:
                 num_islands += 1
-                dfs(num_rows, num_cols, input_array, x, y)
+                dfs(num_rows, num_cols, tiles, x, y)
     return num_islands
--- nfadeeva/assignment4/tests.py
+++ nfadeeva/assignment4/tests.py
@@ -4,31 +4,27 @@ import unittest
 
 class Test(unittest.TestCase):
     def test_example(self):
-        input_array = [[False, True, False, True],
+        tiles = [[False, True, False, True],
                        [True, True, False, False],
                        [False, False, True, False],
                        [False, False, True, False]]
-        num_rows = 4
-        num_cols = 4
-        self.assertEqual(count_islands(num_rows, num_cols, input_array), 3)
+        num_rows, num_cols = 4, 4
+        self.assertEqual(count_islands(num_rows, num_cols, tiles), 3)
 
     def test_empty_array(self):
-        input_array = []
-        num_rows = 0
-        num_cols = 0
-        self.assertEqual(count_islands(num_rows, num_cols, input_array), 0)
+        tiles = []
+        num_rows, num_cols = 0, 0
+        self.assertEqual(count_islands(num_rows, num_cols, tiles), 0)
 
     def test_no_islands(self):
-        num_rows = 4
-        num_cols = 3
-        input_array = [[False for i in range(num_cols)] for i in range(num_rows)]
-        self.assertEqual(count_islands(num_rows, num_cols, input_array), 0)
+        num_rows, num_cols = 4, 3
+        tiles = [[False for i in range(num_cols)] for i in range(num_rows)]
+        self.assertEqual(count_islands(num_rows, num_cols, tiles), 0)
 
     def test_one_island(self):
-        num_rows = 4
-        num_cols = 3
-        input_array = [[True for i in range(num_cols)] for i in range(num_rows)]
-        self.assertEqual(count_islands(num_rows, num_cols, input_array), 1)
+        num_rows, num_cols = 4, 3
+        tiles = [[True for i in range(num_cols)] for i in range(num_rows)]
+        self.assertEqual(count_islands(num_rows, num_cols, tiles), 1)
 
 if __name__ == '__main__':
     unittest.main()

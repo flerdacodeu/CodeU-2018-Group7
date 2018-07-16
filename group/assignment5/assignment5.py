cf568be7dec5e307a2847f438944855453a80635
Ema Pajić <ema.p25@gmail.com>
2018-07-16 15:35:40 +0200

    Update assignment5.py

--- group/assignment5/assignment5.py
+++ group/assignment5/assignment5.py
@@ -5,12 +5,12 @@
 """
 from Graph import GraphNode
 
-def find_first_different_letter(word1,word2):
-    #returns position in words of first different letter, -1 is words are same (or atleast same until the end of the shorter word)
-    length_of_word1 = len(word1)
-    length_of_word2 = len(word2)
+def find_first_different_letter(word1, word2):
+    """returns position in words of first different letter,
+    -1 is words are same (or atleast same until the end of the shorter word)
+    """
     i = 0
-    while i < length_of_word1 and i < length_of_word2:
+    while i < len(word1) and i < len(word2):
         if word1[i] != word2[i]:
             return i
         i += 1
@@ -35,13 +35,15 @@ def topsort(lettersGraph):
                 queue.append(lettersGraph[child])
                 qlen += 1
         i += 1
-    #if dictionary is consistent we will go through all lettersGraph, so if we haven't done that dictionary is inconsistent
+    """if dictionary is consistent we will go through all lettersGraph,
+    so if we haven't done that dictionary is inconsistent
+    """
     if qlen < len(lettersGraph):
         raise BaseException("The given dictionary is inconsistent")
     return alphabet
 
 def find_alphabet(dictionary):
-    """function that finds alphabet of given dictionary.It will return just one of the valid dictionaries.
+    """finds alphabet of given dictionary.It will return just one of the valid dictionaries.
     This function created directed graph where nodes are lettersGraph and edges are relations between lettersGraph
     (edge from a to b means that a is before b)
     """

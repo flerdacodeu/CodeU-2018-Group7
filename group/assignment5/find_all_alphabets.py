18f8ed40c02aa4ff41411470884d2892910b2dac
Smirnova Alisa <aliskin@exascale.info>
2018-07-23 11:56:52 +0200

    Docstrings for find_all_alphabets

--- group/assignment5/find_all_alphabets.py
+++ group/assignment5/find_all_alphabets.py
@@ -3,6 +3,15 @@ from assignment5.find_alphabet import build_graph
 
 
 def find_all_alphabets(dictionary):
+    """Given a lexicographically ordered list of words finds all possible alphabets
+    
+    Args:
+        dictionary: lexicographically ordered list of words
+
+    Returns:
+        A set of all possible alphabets represented as strings.
+    """
+
     graph = build_graph(dictionary)
     letters = graph.vertices
     letters_to_add = list()
@@ -18,6 +27,16 @@ def find_all_alphabets(dictionary):
 
 def find_all_alphabets_with_prefix(graph, prefix, letters_to_add, added_parents,
                                    all_alphabets):
+    """Recursively finds all alphabets with a given prefix
+
+    Args:
+        graph: Graph instance corresponding to a given dictionary
+        prefix: string representing a current prefix of the alphabet
+        letters_to_add: list of letters that can be added after current prefix
+        added_parents: a dict that for each letter store number of its parents that are in the
+            prefix
+        all_alphabets: list of the all alphabets found
+    """
     if len(prefix) == len(graph.vertices):
         all_alphabets.append(prefix)
         return
@@ -32,7 +51,7 @@ def find_all_alphabets_with_prefix(graph, prefix, letters_to_add, added_parents,
         for c in children:
             added_parents_new[c] += 1
             # if we added all parents of p then we add p into letters_to_add
-            if added_parents_new[c] == len(graph.parents[c]): #and (not p in prefix_new):
+            if added_parents_new[c] == len(graph.parents[c]):
                 letters_to_add_new.append(c)
         find_all_alphabets_with_prefix(graph, prefix_new, letters_to_add_new,
                                        added_parents_new, all_alphabets)

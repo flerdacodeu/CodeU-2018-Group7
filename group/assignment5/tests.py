df75a500542dfc49820304dec0aee32be8e8e959
Elizaveta Kuzmenko <7359847+lizaku@users.noreply.github.com>
2018-07-23 15:54:58 +0200

    trying to make output consistent

--- group/assignment5/tests.py
+++ group/assignment5/tests.py
@@ -7,8 +7,7 @@ from assignment5.find_all_alphabets import find_all_alphabets
 class Test(unittest.TestCase):
     def test_example(self):
         input_dict = ['ART', 'RAT', 'CAT', 'CAR']
-        self.assertIn(find_alphabet(input_dict),[['A', 'T', 'R', 'C'],
-                                                 ['T', 'A', 'R', 'C']])
+        self.assertIn(find_alphabet(input_dict), {'ATRC' 'TARC'})
 
     def test_empty_dict(self):
         input_dict = []
@@ -30,7 +29,7 @@ class Test(unittest.TestCase):
 
     def test_alphabet(self):
         input_dict = [s for s in string.ascii_uppercase]
-        self.assertEqual(input_dict, find_alphabet(input_dict))
+        self.assertEqual(string.ascii_uppercase, find_alphabet(input_dict))
 
     def test_alphabet_constraints(self):
         input_dict = [s for s in string.ascii_uppercase]

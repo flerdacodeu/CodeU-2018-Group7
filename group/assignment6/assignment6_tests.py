c7e9cf963bedbb11f6d26a11fda6fbf5000201b3
Elizaveta Kuzmenko <7359847+lizaku@users.noreply.github.com>
2018-07-25 19:03:56 +0200

    add more tests

--- group/assignment6/assignment6_tests.py
+++ group/assignment6/assignment6_tests.py
@@ -8,12 +8,40 @@ class Test(unittest.TestCase):
         self.end_state = [3, 1, 2, 0]
         
     def test_compute_moves(self):
+        # test the simple function
         moves = compute_moves(self.start_state, self.end_state)
         self.assertEqual(apply_moves(self.start_state, moves), self.end_state)
 
     def test_compute_efficient_moves(self):
+        # test the efficient function, challenge #2
         moves = compute_efficient_moves(self.start_state, self.end_state)
         self.assertEqual(apply_moves(self.start_state, moves), self.end_state)
+    
+    # The next two test cases can be applied either to compute_moves() 
+    # or to compute_efficient_moves()
+    def test_two_empty_states(self):
+        # test if both states are empty
+        start_state = []
+        end_state = []
+        moves = compute_efficient_moves(start_state, end_state)
+        self.assertRaises(StopIteration, lambda: next(moves))
+    
+    def test_empty_state(self):
+        # test if one of the states is empty
+        end_state = []
+        moves = compute_efficient_moves(self.start_state, end_state)
+        self.assertRaises(IndexError, lambda: next(moves))
+    
+    def test_apply_to_empty(self):
+        # test applying the moves to the empty start state
+        start_state = []
+        moves = compute_moves(self.start_state, self.end_state)
+        self.assertRaises(IndexError, lambda: apply_moves(start_state, moves))
+    
+    def test_apply_empty_moves(self):
+        # test applying an empty sequence of moves to the start state
+        moves = []
+        self.assertEqual(apply_moves(self.start_state, moves), self.start_state)
 
 
 if __name__ == '__main__':

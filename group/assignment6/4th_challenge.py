82d7225516887161944714be8846e086d495416d
Elizaveta Kuzmenko <7359847+lizaku@users.noreply.github.com>
2018-07-27 23:38:52 +0200

    rework the finding paths algorithms, add 3rd challenge

--- group/assignment6/4th_challenge.py
+++ group/assignment6/4th_challenge.py
@@ -42,6 +42,21 @@ class Graph:
         visited[start] = False
         return all_paths
 
+    
+    def find_all_paths(self, start, end, path = []):
+        path = path + [start]
+        if start == end:
+            return [path]
+        paths = []
+        for node in self.edges[start]:
+            if node not in path:
+                newpaths = self.find_all_paths(node, end, path)
+                for newpath in newpaths:
+                    paths.append(newpath)
+        return paths
+    
+    def decode_path(self, path):
+        return [self.nums_to_states[x] for x in path]
 
 def find_empty(current_state):
     """
@@ -50,7 +65,7 @@ def find_empty(current_state):
     return current_state.index(0)
 
 
-def build_graph(num_places, constraints=[]):
+def build_graph(num_places, constraints={}):
     graph = Graph()
     all_permutations = permutations(range(num_places))
     graph.nums_to_states = dict(enumerate(map(list, all_permutations)))
@@ -62,15 +77,21 @@ def build_graph(num_places, constraints=[]):
         for i in range(num_places):
             if i != empty:
                 new_state = state.copy()
+                if len(constraints) > 0:
+                    if new_state[i] not in constraints[i]:
+                        continue
                 new_state[empty], new_state[i] = new_state[i], new_state[empty]
                 graph.edges[num].append(graph.states_to_nums[tuple(new_state)])
 
     return graph
 
 if __name__ == '__main__':
-    start_state = (0, 1, 2,3)
-    end_state = (2, 1, 0,3)
-    g = build_graph(4)
-    for path in g.print_all_paths(g.states_to_nums[start_state],\
-                                  g.states_to_nums[end_state]):
-        print(path)
+    start_state = (1, 2, 0, 3)
+    end_state = (3, 1, 2, 0)
+    constraints = {0: (0, 1, 2, 3), 1: (0, 1, 2), 2: (0, 1, 3), 3: (0, 1, 2, 3)}
+    g = build_graph(4, constraints)
+    my_paths = g.find_all_paths(g.states_to_nums[start_state], g.states_to_nums[end_state])
+    decoded_paths = [g.decode_path(path) for path in my_paths]
+    print(decoded_paths)
+    old_paths =  g.print_all_paths(g.states_to_nums[start_state], g.states_to_nums[end_state])
+    print(len(my_paths), len(old_paths))

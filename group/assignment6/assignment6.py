e0bcac2b188dc55c66b28f1e2a71d88e5724adc8
Anastasia Fadeeva <a.fadeeva@phoenixcas.com>
2018-08-03 17:13:03 +0300

    Add fixes

--- group/assignment6/assignment6.py
+++ group/assignment6/assignment6.py
@@ -7,11 +7,11 @@ Thus, a number of a lot in which a car is placed is an index of that car
 number in the list. For example, [1, 2, 0, 3] is the start state. 
 Car #1 is in the lot #0, car #2 is in the lot #1, etc.
 """
-
+from path_finder import PathFinder
 if __name__ == '__main__':
     start_state = [1, 2, 0, 3] 
     end_state = [3, 1, 2, 0]
     path_finder = PathFinder(start_state, end_state)
-    moves = list(path_finder.compute_moves())
+    moves = list(path_finder.compute_efficient_moves())
     print(moves)
     print(path_finder.apply_moves(moves))

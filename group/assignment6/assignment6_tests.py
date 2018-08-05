ffd122b3ba3b88629d7455d692feee28b84a5d73
Anastasia Fadeeva <a.fadeeva@phoenixcas.com>
2018-08-05 22:13:09 +0300

    Fixes

--- group/assignment6/assignment6_tests.py
+++ group/assignment6/assignment6_tests.py
@@ -80,7 +80,7 @@ class TestInput(unittest.TestCase):
         start_state = [0, 1, 2, 3]
         end_state = [0, 2, 1, 3]
         constraints = {0: (1, 3), 1: (0, 1, 2, 3), 2: (0, 1, 2, 3), 3: (0, 1, 2, 3)}
-        self.assertRaises(ValueError, lambda : PathFinder(start_state, end_state, constraints))
+        self.assertRaises(ValueError, lambda: PathFinder(start_state, end_state, constraints))
 
 
 class TestEfficientFunction(unittest.TestCase):
